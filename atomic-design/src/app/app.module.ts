c0ab1e1656d9b5b8269f712e998a5c8b0e5f4689
Jorddi Suarez <jorddi.suarez@pragma.co>
2023-06-07 11:08:04 -0500

    atomos y modeluclas de header y banner

--- atomic-design/src/app/app.module.ts
+++ atomic-design/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './components/atoms/logo/logo.component';
+import { HeaderComponent } from './components/organisms/header/header.component';
+import { HomeComponent } from './components/templates/home/home.component';
+import { BannerComponent } from './components/organisms/banner/banner.component';
+import { ImagesBannerComponent } from './components/molecules/images-banner/images-banner.component';
+import { PrincipalBottonComponent } from './components/atoms/principal-botton/principal-botton.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        BannerComponent,
+        ImagesBannerComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HeaderComponent,
+        LogoComponent,
+        PrincipalBottonComponent
+    ]
 })
 export class AppModule { }
